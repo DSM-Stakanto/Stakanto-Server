030063486192a24776de373db33e968550767c2a
luck1306 <luck1306@naver.com>
2023-01-21 18:14:11 +0900

    Initial commit

---
+++ src/main/java/com/server/stakantoserver/StakantoServerApplication.java
@@ -0,0 +1,13 @@
+package com.server.stakantoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StakantoServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StakantoServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/server/stakantoserver/StakantoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.stakantoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StakantoServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
