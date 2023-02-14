64cdacdb7067bbd71cbc18fdc057a513d118ecdc
luck1306 <luck1306@naver.com>
2023-02-15 07:32:33 +0900

    update SecurityConfig(inject passwordEncoder to the Bean)

--- src/main/java/com/server/stakantoserver/security/SecurityConfig.java
+++ src/main/java/com/server/stakantoserver/security/SecurityConfig.java
@@ -1,16 +1,27 @@
 package com.server.stakantoserver.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final TokenProvider tokenProvider;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http
@@ -21,6 +32,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().authorizeRequests()
                 .antMatchers("/auth/**").permitAll()
-                .anyRequest().permitAll();
+                .anyRequest().permitAll()
+
+                .and()
+                .addFilter(new TokenFilter(tokenProvider));
     }
 }
