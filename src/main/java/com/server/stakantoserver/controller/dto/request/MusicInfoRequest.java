b62a4ca1a8f7a560538d4978d1f2b5d867966c38
luck1306 <luck1306@naver.com>
2023-02-21 19:35:36 +0900

    update

---
+++ src/main/java/com/server/stakantoserver/controller/dto/request/MusicInfoRequest.java
@@ -0,0 +1,18 @@
+package com.server.stakantoserver.controller.dto.request;
+
+import com.server.stakantoserver.entity.Hint;
+import lombok.Getter;
+
+@Getter
+public class MusicInfoRequest {
+
+    private String answer;
+
+    private String code;
+
+    private String name;
+
+    private int startAt;
+
+    private Hint hint;
+}
