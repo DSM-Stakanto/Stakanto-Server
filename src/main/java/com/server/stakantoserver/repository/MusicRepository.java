d7cb6b5b584b62c8c23d53a59ff49be82c7d96d5
luck1306 <luck1306@naver.com>
2023-02-21 20:42:10 +0900

    update provide music list

--- src/main/java/com/server/stakantoserver/repository/MusicRepository.java
+++ src/main/java/com/server/stakantoserver/repository/MusicRepository.java
@@ -3,8 +3,11 @@ package com.server.stakantoserver.repository;
 import com.server.stakantoserver.entity.Music;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface MusicRepository extends CrudRepository<Music, Long> {
     Optional<Music> findByName(String name);
+
+    List<Music> findByGenre(String genre);
 }
